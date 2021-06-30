a750e6395c3303dcc20834753c23604254f1af5d
Francisco Schulz <22344801+fschlz@users.noreply.github.com>
2021-06-30 10:07:43 +0200

    Update loot.py

--- src/sailoot/loot.py
+++ src/sailoot/loot.py
@@ -2,8 +2,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-
-    os.system("source /usr/local/anaconda3/bin/activate && conda activate py37")
     
     path = '../resources/questions.json'
 
